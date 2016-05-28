facc44950286f82d450a49ca6ea6c5fdb9cd92ad
Nikolay Rybak <v-nrybak@expedia.com>
2016-05-28 21:57:16 +0300

    Simple script to automate version updates

---
+++ update-version.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [ $# -ne 1 ]
+then
+    echo "Requires single parameter: AWS CLI version"
+    exit 1
+fi
+
+new_version="$1"
+
+sed -i.bak "s/awscli==[0-9.]* /awscli==${new_version} /" Dockerfile
+rm Dockerfile.bak
+git add Dockerfile
+git commit --no-edit -m "Update AWS CLI version to ${new_version}"
+git tag "${new_version}"
+git push --follow-tags
