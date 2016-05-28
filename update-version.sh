9341e5c5ad75d4fcd79b8d262b7a1c798c6ed6c3
Nikolay Rybak <v-nrybak@expedia.com>
2016-05-28 22:14:25 +0300

    Add test phase to script

--- update-version.sh
+++ update-version.sh
@@ -13,6 +13,10 @@ new_version="$1"
 
 sed -i.bak "s/awscli==[0-9.]* /awscli==${new_version} /" Dockerfile
 rm Dockerfile.bak
+
+docker build -t awscli-test .
+docker rmi awscli-test
+
 git add Dockerfile
 git commit --no-edit -m "Update AWS CLI version to ${new_version}"
 git tag "${new_version}"
